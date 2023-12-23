81a9776eccbde5083d2c3cb1ba35a92147d0f563
teleoflexuous <teleoflexuous@proton.me>
2023-12-23 17:12:10 +0100

    Revert "day 6, part 1 solved"
    This reverts commit be617caf33aa7975c8450abde7eb5ce513defddd.

--- day6/target/debug/day6.d
+++
@@ -1,1 +0,0 @@
-F:\PyDocs\AoC-Rust\day6\target\debug\day6.exe: F:\PyDocs\AoC-Rust\day6\src\main.rs
--- day6/target/debug/deps/day6.d
+++
@@ -1,5 +0,0 @@
-F:\PyDocs\AoC-Rust\day6\target\debug\deps\day6.exe: src\main.rs
-
-F:\PyDocs\AoC-Rust\day6\target\debug\deps\day6.d: src\main.rs
-
-src\main.rs:
